61a426f4d916a35da62331cc578f412323b2b79b
Sarah Usher <sarahnusher@gmail.com>
2021-06-09 19:33:37 +0100

    GRREEEEEENNN

--- main/tennisGame.js
+++ main/tennisGame.js
@@ -4,11 +4,9 @@ class TennisGame {
     let player1 = { Name: "Ben", Score: 0 };
     let player2 = { Name: "Maria", Score: 0 };
 
-    players[0] = player1;
-    players[0] = player1;
-    const retValue = { players: players };
-
-    return retValue;
+    players[0] = {player1};
+    players[1] = {player2};
+    return { players: players };
   }
 }
 module.exports = TennisGame;
--- test/tennisGame.test.js
+++ test/tennisGame.test.js
@@ -1,24 +1,25 @@
 const TennisGame = require("../main/tennisGame");
 
 test("each player has 0 points at the beginning of the game", () => {
-  const game = new TennisGame();
-  const points = game.getScore();
+    const game = new TennisGame();
+    const points = game.getScore();
 
-  const expectedPoints = {
-    players: [
+    const expectedPoints = {
+        players: [
+            {
+                player1: {
+                    Name: "Ben",
+                    Score: 0,
+                }
+            },
+            {
+                player2: {
+                    Name: "Maria",
+                    Score: 0,
+                }
+            }
+        ]
+    };
 
-        player1: {
-          Name: "Ben",
-          Score: 0,
-        },
-
-        player2: {
-          Name: "Maria",
-          Score: 0,
-        },
-
-    ],
-  };
-
-  expect(points).toBe(expectedPoints);
+    expect(points).toStrictEqual(expectedPoints);
 });
