dd5f459b3d41b8615265236543271449f6ee8645
Joanna Czerwinska <joczerw@gmail.com>
2021-06-09 18:41:20 +0100

    Modified a test

--- test/tennisGame.test.js
+++ test/tennisGame.test.js
@@ -1,8 +1,11 @@
 const TennisGame = require("../main/tennisGame");
 
-test('input empty string and return 0', () => {
-    calculator = new TennisGame()
-    const result = calculator.add("")
+test('each player has 0 points at the beginning of the game', () => {
+    const game = new TennisGame();
+    const points = game.getScore();
+
+    // calculator = new TennisGame()
+    // const result = calculator.add("")
 
     expect(result).toBe(0)
 });
